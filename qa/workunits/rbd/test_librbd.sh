a060ebe229a81b964e0167c47de03e60ea55dd12
Matan Breizman <mbreizma@redhat.com>
2022-12-04 12:39:52 +0000

    filter
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- qa/workunits/rbd/test_librbd.sh
+++ qa/workunits/rbd/test_librbd.sh
@@ -2,8 +2,8 @@
 
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
-    --error-exitcode=1 ceph_test_librbd
+    --error-exitcode=1 ceph_test_librbd --gtest_filter="TestDeepCopy.*"
 else
-  ceph_test_librbd
+  ceph_test_librbd --gtest_filter="TestDeepCopy.*"
 fi
 exit 0
